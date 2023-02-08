85be8f5bf48f47a617c0b15b3d7f2cd0f85226d2
Eduardaskuliesa <eduardas2000@mail.ru>
2023-02-08 08:15:29 +0200

    feat(6) completed 1-10

--- Material Ui 1-10/src/pages/mui-select-page.tsx
+++ Material Ui 1-10/src/pages/mui-select-page.tsx
@@ -1,7 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const MuiSelectPage = () => (
-  <div> hello</div>
-);
+import { Box, TextField, MenuItem } from '@mui/material';
+
+export const MuiSelectPage = () => {
+  const [countries, setCountries] = useState<string[]>([]);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+    setCountries(typeof value === 'string' ? value.split(',') : value);
+  };
+  return (
+    <Box width="250px">
+      <TextField
+        label="Select country"
+        select
+        SelectProps={{
+          multiple: true,
+        }}
+        size="small"
+        color="secondary"
+        helperText="Please select your country"
+        value={countries}
+        onChange={handleChange}
+      >
+        <MenuItem value="IN">India</MenuItem>
+        <MenuItem value="US">USA</MenuItem>
+        <MenuItem value="AU">Australia</MenuItem>
+      </TextField>
+    </Box>
+  );
+};
 
 export default MuiSelectPage;
